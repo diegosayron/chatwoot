c414c93b179ea27655e31eb3e08b0e0eb8075b25
Diego Sayron <diegosayron@gmail.com>
2025-08-08 12:40:57 -0300

    EnterpriseAdminpatch alterado

--- config/initializers/enterprise_admin_patch.rb
+++ config/initializers/enterprise_admin_patch.rb
@@ -1,9 +1,3 @@
-Rails.application.config.to_prepare do
-  if defined?(Enterprise::Account::PlanUsageAndLimits)
-    Enterprise::Account::PlanUsageAndLimits.prepend(EnterpriseAdminPlanOverride)
-  end
-end
-
 module EnterpriseAdminPlanOverride
   def plan_name
     return "Enterprise" if self.id == 1
@@ -16,11 +10,24 @@ module EnterpriseAdminPlanOverride
         inbound_emails help_center campaigns team_management channel_twitter
         channel_facebook channel_email channel_instagram captain_integration
         sla custom_roles audit_logs disable_branding response_bot
-        help_center_embedding_search captain_integration_v2
+        help_center_embedding_search captain_integration_v2 custom_branding
+        custom_widgets custom_reports webhooks advanced_automation
       ]
     else
       super
     end
+  end
+
+  def feature_enabled?(*)
+    self.id == 1 ? true : super
+  end
+
+  def has_feature?(*)
+    self.id == 1 ? true : super
+  end
+
+  def ee?
+    self.id == 1 ? true : (defined?(super) ? super : false)
   end
 
   def usage_limits
@@ -29,5 +36,11 @@ module EnterpriseAdminPlanOverride
     else
       super
     end
+  end
+end
+
+Rails.application.config.to_prepare do
+  if defined?(Enterprise::Account::PlanUsageAndLimits)
+    Enterprise::Account::PlanUsageAndLimits.prepend(EnterpriseAdminPlanOverride)
   end
 end
